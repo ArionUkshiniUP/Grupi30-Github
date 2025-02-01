abe25dee958911e31b8ed6b505f7ea157a879f2b
ArionUkshiniUP <arion.ukshini@student.uni-pr.edu>
2025-02-01 15:16:49 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "menu.cpp"
+
+using namespace std;
+
+int main()
+{
+    bool loop = true;
+    
+    while (loop)
+    {
+        int opsioni = shfaqMenu();
+    }
+}
