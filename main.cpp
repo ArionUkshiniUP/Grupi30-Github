ed89e4d9177d2f0f24d39cc011aadd395ce2a16c
edisonukshini1 <edison.ukshini@student.uni-pr.edu>
2025-02-02 16:04:39 +0100

    Update main.cpp
    Programi tani thirr funksionin menu dhe nxjerr opsionin e vendosur nga perdoruesi.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <fstream>
+#include "menu.cpp"
 
 using namespace std;
 
 int main()
 {
-  return 0;
+    bool loop = true;
+    
+    while (loop)
+    {
+        int opsioni = shfaqMenu();
+        ifstream inFile("detyrat.txt"); // Hap skedarin për lexim
+        ofstream outFile; // Hapet më vonë nëse është e nevojshme
+    }
 }
