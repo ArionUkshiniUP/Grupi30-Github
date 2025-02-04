47d1b90087002fd7b175e96d3fe3d2aa6ae126f3
bardhbibajuni <bardh.bibaj@student.uni-pr.edu>
2025-02-04 14:08:36 +0100

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <windows.h>
+#include <unistd.h>
+#include "menu.cpp"
+
+
+using namespace std;
+
+
+int main()
+{
+    bool loop = true;
+    
+    while (loop)
+    {
+        int opsioni = shfaqMenu();
+        ifstream inFile("detyrat.txt");
+        ofstream outFile;
+        vector<string> listaDetyrave;
+        string detyra;
+        string detyrat;
+
+
+        switch (opsioni)
+        {
+        case 1: // Shfaq detyrat
+
+
+        case 2: // Shto detyre
+            cout << "\nShto detyren: ";
+            cin.ignore(); // Pas `cin` nevojitet `ignore()`
+            getline(cin, detyra);
+
+
+            outFile.open("detyrat.txt", ios::app); // Hap për shtim
+            outFile << detyra << "\n";
+            outFile.close();
+            
+            cout << "\nDetyra u shtua me sukses!\n";
+            sleep(1);
+            break;
+
+
+        case 3: // Fshi detyre
+
+
+        case 4: // Dalje nga programi
+        }
+    }
+
+
+    return 0;
+}
+
+
+
